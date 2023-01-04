486fd8507a51f4b387eb23632ca2c9990a299aff
zahrablend <zahra@LAPTOP-010BK67M>
2023-01-04 23:00:18 +0200

    Added logic to find current day of the week and convert to internal data

--- script.py
+++ script.py
@@ -1,9 +1,28 @@
 # list days of the week
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 # keywords used in menu items selection
 # meat, poultry, fish, seafood, vegetarian, dessert
 # define test customer: name, day, selection criteria
-test_customer = ['Anna Wild', 'Tuesday', ['vegetarian', 'dessert']]
+test_customer = ['Anna Wild', 'Monday', ['vegetarian', 'dessert']]
+
+# get index of menu day
+def get_day_index(day):
+    day_index = week_days.index(day)
+    return day_index
+
+# test case 1: get index of week_days(day)
+# print(get_day_index('Wednesday'))
+
+# get customer current day
+def get_customer_current_day(customer):
+    current_day = customer[1]
+    current_day_index = get_day_index(current_day)
+    return current_day_index
+
+# test case 2: check if customer's current day matches the week_days index
+# test_current_day_index = get_customer_current_day(test_customer)
+# print(test_current_day_index)
+
 
 
 
