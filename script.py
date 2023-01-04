9b3f3218a868081330d25797b1a210c6fa6a3e4a
zahrablend <zahra@LAPTOP-010BK67M>
2023-01-04 22:45:10 +0200

    Added test objects

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
-#list days of the week
+# list days of the week
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+# keywords used in menu items selection
+# meat, poultry, fish, seafood, vegetarian, dessert
+# define test customer: name, day, selection criteria
+test_customer = ['Anna Wild', 'Tuesday', ['vegetarian', 'dessert']]
+
+
+
+
