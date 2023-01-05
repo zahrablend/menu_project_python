14d5b9ac9b3a80edb698cdfd9626bf370366f559
zahrablend <zahra@LAPTOP-010BK67M>
2023-01-05 20:45:41 +0200

    Added function to generate message for customer on available menu

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ def get_week_day_index(week_day):
     week_day_index = week_days.index(week_day)
     return week_day_index
 
-# test case 1: get index of week_days(day)
+# TEST CASE 1: get index of week_days(day)
 # print(get_week_day_index('Wednesday'))
 
 # get current day for customer
@@ -17,7 +17,7 @@ def get_current_day(customer):
     customer_current_day_index = get_week_day_index(current_day) 
     return customer_current_day_index
 
-# test case 2: check if customer's current day matches the week_days index
+# TEST CASE 2: check if customer's current day matches the week_days index
 # test_current_day_index = get_current_day(test_customer)
 # print(test_current_day_index)
 
@@ -34,24 +34,24 @@ def add_menu_item(week_day, menu_item):
     except SyntaxError:
         return
 
-add_menu_item('Monday', ["Slow Cooker Texas Pulled Pork", ['meat']])
-add_menu_item('Tuesday', ["Quick Beef Stir-Fry", ['meat', 'veggies']])
-add_menu_item('Thursday', ["Slow-Cooker Corned Beef and Cabbage", ['meat', 'veggies']])
+add_menu_item('Monday', ["Slow Cooker Texas Pulled Pork", ['meat', 'main']])
+add_menu_item('Tuesday', ["Quick Beef Stir-Fry", ['meat', 'veggies', 'main']])
+add_menu_item('Thursday', ["Slow-Cooker Corned Beef and Cabbage", ['meat', 'veggies', 'main']])
 add_menu_item('Saturday', ["Chef John's Perfect Prime Rib", ['meat']])
 
-add_menu_item('Monday', ["Chicken Stir-Fry", ['poultry', 'veggies']])
-add_menu_item('Wednesday', ["Frito Chicken Casserole", ['poultry']])
-add_menu_item('Friday', ["Cast Iron Roast Chicken and Potatoes", ['poultry']])
-add_menu_item('Sunday', ["Broccoli Chicken Divan", ['poultry', 'veggies']])
+add_menu_item('Monday', ["Chicken Stir-Fry", ['poultry', 'veggies', 'main']])
+add_menu_item('Wednesday', ["Frito Chicken Casserole", ['poultry', 'main']])
+add_menu_item('Friday', ["Cast Iron Roast Chicken and Potatoes", ['poultry', 'main']])
+add_menu_item('Sunday', ["Broccoli Chicken Divan", ['poultry', 'veggies', 'main']])
 
-add_menu_item('Tuesday', ["Easy Paella", ['fish', 'seafood']])
-add_menu_item('Monday', ["Baked Haddock with side veggies", ['fish', 'veggies']])
-add_menu_item('Saturday', ["The Best Thai Coconut Soup", ['fish', 'seafood']])
+add_menu_item('Tuesday', ["Easy Paella", ['fish', 'seafood', 'main']])
+add_menu_item('Monday', ["Baked Haddock with side veggies", ['fish', 'veggies', 'main']])
+add_menu_item('Saturday', ["The Best Thai Coconut Soup", ['fish', 'seafood', 'main']])
 
 add_menu_item('Monday', ["Vegetarian Pasta Salad", ['vegetarian', 'salad']])
-add_menu_item('Tuesday', ["Vegetarian Curry", ['vegetarian']])
-add_menu_item('Friday', ["Pub-Style Vegetarian Chili", ['vegetarian']])
-add_menu_item('Saturday', ["Avocado Watermelon Salad", ['vegetarian', 'veggies']])
+add_menu_item('Tuesday', ["Vegetarian Curry", ['vegetarian', 'main']])
+add_menu_item('Friday', ["Pub-Style Vegetarian Chili", ['vegetarian', 'main']])
+add_menu_item('Saturday', ["Avocado Watermelon Salad", ['vegetarian', 'veggies', 'main']])
 
 add_menu_item('Thursday', ["Best Chicken Salad", ['salad', 'poultry']])
 add_menu_item('Saturday', ["Beet Salad with Goat Cheese", ['salad', 'vegetarian']])
@@ -63,8 +63,6 @@ add_menu_item('Monday', ["Chocolate ice-cream", ['dessert', 'chocolate']])
 add_menu_item('Tuesday', ["Fresh fruit bowl", ['dessert', 'fruit']])
 add_menu_item('Wednesday', ["American chocolate chip cookies", ['dessert', 'chocolate']])
 add_menu_item('Thursday', ["Fruit mix ice-cream", ['dessert', 'fruit']])
-
-print(menu_items)
 
 # find menu items, available on a particular day and match with customer interests
 def find_menu_items(week_day, interests):
@@ -83,10 +81,29 @@ def find_menu_items(week_day, interests):
 
     return menu_items_matching_interest
 
-# test case: check if function call returns correct results
-dessert = find_menu_items('Tuesday', ['dessert'])
-friday_meal = find_menu_items('Friday', ['fish'])
-veggies = find_menu_items('Monday', ['veggies'])
-print(dessert)
-print(friday_meal)
-print(veggies)
+# TEST CASE 3: check if function call returns correct results
+# dessert = find_menu_items('Tuesday', ['dessert'])
+# friday_meal = find_menu_items('Friday', ['fish'])
+# veggies = find_menu_items('Monday', ['veggies'])
+# print(dessert)
+# print(friday_meal)
+# print(veggies)
+
+# separate customer's data
+def get_menu_items_for_customer(customer):
+    customer_week_day = customer[1]
+    customer_interests = customer[2]
+    customer_menu_items = find_menu_items(customer_week_day,  customer_interests)
+
+    communication_string = "Welcome " + customer[0] + ", this " + customer_week_day + " we can offer you "
+
+    for i in range(len(customer_menu_items)):
+        if customer_menu_items[-1] == customer_menu_items[i]:
+            communication_string += customer_menu_items[i] + ". Bon appétit!"
+        else:
+            communication_string += customer_menu_items[i] + " and "
+    
+    return communication_string
+
+customer_marvin = get_menu_items_for_customer(['Mr. Marvin', 'Saturday', ['meat', 'fish']])
+print(customer_marvin)
